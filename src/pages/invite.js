9db38551db06070339a2e7d0dc246089379dea1d
AnimaFPS <lukecsnz@gmail.com>
2021-04-06 21:23:08 +1200

    Troubleshooting

--- src/pages/invite.js
+++ src/pages/invite.js
@@ -1,7 +1,10 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 export default function Invite() {
   useEffect(() => {
     window.location.href = "https://top.gg/bot/792712521546465301/invite/";  
   }, []);
+  return (
+      <p>yep</p>
+  )
 }
